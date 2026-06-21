b42c7431faad1d9d3aead27fc7eb835a8c90b609
Praveen raj <pr186360@gmail.com>
2026-06-21 13:52:43 +0530

    Fix duplicate home route and add login route
    Removed duplicate home route and added login route.

--- app.py
+++ app.py
@@ -5,4 +5,12 @@ app = Flask(__RAILWAY-MANAGEMENT-SYSTEM__)
 @app.route("/")
 def home():
     return "Deployment successful"
+    @app.route("/")
+def home():
+    return "Hello World"
+
+
+@app.route("/login")
+def login():
+    return "Login page"
     
