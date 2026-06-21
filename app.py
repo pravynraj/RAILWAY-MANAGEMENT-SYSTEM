cbd67f91a292b3f9a955dc5c0c768d2b416efae2
Praveen raj <pr186360@gmail.com>
2026-06-21 14:01:04 +0530

    Change home route response to 'OK'

--- app.py
+++ app.py
@@ -4,13 +4,5 @@ app = Flask(__RAILWAY-MANAGEMENT-SYSTEM__)
 
 @app.route("/")
 def home():
-    return "Deployment successful"
-    @app.route("/")
-def home():
-    return "Hello World"
-
-
-@app.route("/login")
-def login():
-    return "Login page"
+    return "OK"
     
